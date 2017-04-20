a6b389639dd3961f707ca5c8f0f159d8a14adc0a
sophanarith sok <sophanarithsok@gmail.com>
2017-04-20 13:15:02 -0700

    Complete POST /users/trade/accept-trade route to allow users to accept trade proposals.

--- routes/trade.js
+++ routes/trade.js
@@ -88,18 +88,18 @@ router.post('/propose-trade/send', authenticate, (req, res) => {
   });
 });
 
-// POST /users/accept-trade to accept a trade from another user.
-router.patch('/accept-trade', authenticate, (req, res) => {
+// POST /users/trade/accept-trade to accept a trade from another user.
+router.post('/accept-trade', authenticate, (req, res) => {
   let tradeId = req.body.tradeId;
   console.log('tradeId: ', tradeId);
   
   // Find trade by id and set 'status' to 'accepted'.
-  // Trade.findByIdAndUpdate(body.tradeId, 
-  //   { $set: { status: 'accepted' } },
-  //   { safe: true, upsert: true, new : true })
-  // .then((updatedTrade) => {
-  //   res.send(updatedTrade);
-  // }).catch((err) => res.status(400).send(err));
+  Trade.findByIdAndUpdate(tradeId, 
+    { $set: { status: 'accepted' } },
+    { safe: true, upsert: true, new : true })
+  .then((updatedTrade) => {
+    res.redirect('/users/mybookshelf');
+  }).catch((err) => res.status(400).send(err));
 });
 
 // POST /users/decline-trade to decline a trade from another user.
